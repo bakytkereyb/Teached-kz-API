34ad216518ee4e2331e97c28e2ba30c5c5821dca
Batyrbek <bakytkerey.b@gmail.com>
2023-03-13 17:57:51 +0600

    first commit

---
+++ src/test/java/diplomka/diplomkaapiapp/DiplomkaApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package diplomka.diplomkaapiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomkaApiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
