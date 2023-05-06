65f1baa3357c3da64283a4f72ada843fa6d50186
Batyrbek <bakytkerey.b@gmail.com>
2023-05-06 15:11:52 +0600

    feat: test controller

---
+++ src/main/java/diplomka/diplomkaapiapp/controllers/TestController.java
@@ -0,0 +1,25 @@
+package diplomka.diplomkaapiapp.controllers;
+
+import diplomka.diplomkaapiapp.services.competence.bank.DeleteCompetenceBankService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/test")
+@Slf4j
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+@Tag(name = "Test", description = "Test")
+public class TestController {
+    private final DeleteCompetenceBankService deleteCompetenceBankService;
+
+//    @GetMapping("/test")
+//    public ResponseEntity getBaty(@RequestParam("id") UUID id) {
+//        return ResponseEntity.ok(deleteCompetenceBankService.getBaty(id));
+//    }
+}
