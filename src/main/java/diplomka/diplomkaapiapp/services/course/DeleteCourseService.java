6dc9c0bac135218606c294b3c0ef4d10686d6886
Batyrbek <bakytkerey.b@gmail.com>
2023-05-09 02:08:14 +0600

    feat: delete course service

---
+++ src/main/java/diplomka/diplomkaapiapp/services/course/DeleteCourseService.java
@@ -0,0 +1,22 @@
+package diplomka.diplomkaapiapp.services.course;
+
+import diplomka.diplomkaapiapp.entities.course.Course;
+import diplomka.diplomkaapiapp.repositories.course.CourseRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class DeleteCourseService {
+    private final CourseRepository courseRepository;
+
+    public void deleteCourse(Course course) {
+        course.setStudents(null);
+        courseRepository.save(course);
+        courseRepository.delete(course);
+    }
+}
