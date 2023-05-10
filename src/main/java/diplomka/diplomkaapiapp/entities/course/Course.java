ec74794ed55f9d90dcaeb678e203f07d0c3d99be
Batyrbek <bakytkerey.b@gmail.com>
2023-05-11 02:45:30 +0600

    feat: course sections, add/remove section

--- src/main/java/diplomka/diplomkaapiapp/entities/course/Course.java
+++ src/main/java/diplomka/diplomkaapiapp/entities/course/Course.java
@@ -1,5 +1,6 @@
 package diplomka.diplomkaapiapp.entities.course;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import diplomka.diplomkaapiapp.entities.user.User;
 import jakarta.persistence.*;
@@ -12,7 +13,9 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "courses")
@@ -62,14 +65,46 @@ public class Course {
 
     private LocalDateTime createdAt = LocalDateTime.now();
 
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @OrderBy("createdAt ASC")
+    private List<CourseSection> sections;
+
+    public void addSection(CourseSection section) {
+        this.sections.add(section);
+    }
+    public void removeSectionById(UUID sectionId) {
+        this.sections = this.sections.stream()
+                .filter(courseSection -> !courseSection.getId().equals(sectionId))
+                .collect(Collectors.toList());
+    }
+
+    @ManyToOne
+    private CourseSection certificateSection;
+
     @ManyToOne
     private User trainer;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JsonIgnore
     private Collection<User> students = new ArrayList<>();
 
-    public Course(String name, String description, String nameKz, String descriptionKz, String nameRu, String descriptionRu, User trainer) {
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Collection<User> closedStudents = new ArrayList<>();
+
+    @Transient
+    private Integer progress;
+    @Transient
+    @JsonGetter
+    public Integer maxProgress() {
+        return sections == null ? 0 : sections.size();
+    }
+
+    @Transient
+    private CourseUserStatus userStatus;
+
+
+
+    public Course(String name, String description, String nameKz, String descriptionKz, String nameRu, String descriptionRu, User trainer, CourseSection certificateSection) {
         this.name = name;
         this.description = description;
         this.nameKz = nameKz;
@@ -77,6 +112,7 @@ public class Course {
         this.nameRu = nameRu;
         this.descriptionRu = descriptionRu;
         this.trainer = trainer;
+        this.certificateSection = certificateSection;
     }
 
     public void addStudent(User student) {
