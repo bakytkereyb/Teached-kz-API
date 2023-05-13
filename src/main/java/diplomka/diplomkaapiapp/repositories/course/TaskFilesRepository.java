ac14bbfdb63ab9c4608ed23aa84d2d9fcdece669
Batyrbek <bakytkerey.b@gmail.com>
2023-05-14 03:13:51 +0600

    feat: task files rep findByTaskAndStudent

--- src/main/java/diplomka/diplomkaapiapp/repositories/course/TaskFilesRepository.java
+++ src/main/java/diplomka/diplomkaapiapp/repositories/course/TaskFilesRepository.java
@@ -1,10 +1,14 @@
 package diplomka.diplomkaapiapp.repositories.course;
 
+import diplomka.diplomkaapiapp.entities.course.Task;
 import diplomka.diplomkaapiapp.entities.course.TaskFiles;
+import diplomka.diplomkaapiapp.entities.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TaskFilesRepository extends JpaRepository<TaskFiles, UUID>, PagingAndSortingRepository<TaskFiles, UUID> {
+    TaskFiles findByTaskAndStudent(Task task, User student);
 }
